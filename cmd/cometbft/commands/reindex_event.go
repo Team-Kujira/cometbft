12c3c1ccfa0d0263451dad0cb443f79ffadf0a70
codehans <94654388+codehans@users.noreply.github.com>
2023-06-27 20:27:03 +0100

    psql fixes

--- cmd/cometbft/commands/reindex_event.go
+++ cmd/cometbft/commands/reindex_event.go
@@ -161,6 +161,10 @@ func eventReIndex(cmd *cobra.Command, args eventReIndexArgs) error {
 				ResultEndBlock:   *r.EndBlock,
 			}
 
+			if err := args.blockIndexer.Index(e); err != nil {
+				return fmt.Errorf("block event re-index at height %d failed: %w", i, err)
+			}
+
 			var batch *txindex.Batch
 			if e.NumTxs > 0 {
 				batch = txindex.NewBatch(e.NumTxs)
@@ -181,10 +185,6 @@ func eventReIndex(cmd *cobra.Command, args eventReIndexArgs) error {
 				if err := args.txIndexer.AddBatch(batch); err != nil {
 					return fmt.Errorf("tx event re-index at height %d failed: %w", i, err)
 				}
-			}
-
-			if err := args.blockIndexer.Index(e); err != nil {
-				return fmt.Errorf("block event re-index at height %d failed: %w", i, err)
 			}
 		}
 
